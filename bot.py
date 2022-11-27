2b93a8ddc68916a8182f1f4938fdbb6442598b37
cinesubzfilebot <95116028+cinesubzfilebot@users.noreply.github.com>
2022-11-27 15:31:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,6 @@ async def start(bot: Client, cmd: Message):
             await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
 
 
-@Bot.on_message((filters.document | filters.video | filters.audio) & ~filters.edited)
 async def main(bot: Client, message: Message):
     if message.chat.type == "private":
         chat_id = message.from_user.id
